1afe40ee3f96ffb9dfc391d903cdca59e6d487ad
coryellenberger <cory.ellen.ce@gmail.com>
2016-08-06 11:04:12 -0700

    - comma-split.filter.js  -- fixing the commaSplit filter format

--- public/assets/filters/comma-split.filter.js
+++ public/assets/filters/comma-split.filter.js
@@ -9,29 +9,21 @@
 
   /**
    * @namespace commaSplit
+   * @desc description
+   * @param {String} input the csv to be split
+   * @returns {Array} split array of string
    * @memberOf Filters
    */
   function commaSplit () {
-    return commaSplitFilter
-
-    /**
-     * @name commaSplitFilter
-     * @desc description
-     * @param {String} input the csv to be split
-     * @returns {Array} split array of string
-     * @memberOf Filters.commaSplit
-     */
-    function commaSplitFilter () {
-      return function (input) {
-        if (!input) {
-          return
-        }
-        var split = input.split(',')
-        if (split[0] === '') {
-          return
-        }
-        return split
+    return function (input) {
+      if (!input) {
+        return
       }
+      var split = input.split(',')
+      if (split[0] === '') {
+        return
+      }
+      return split
     }
   }
 })()
